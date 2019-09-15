6bf96cf63b05550a93891930626529a46cec9b42
Ercan-home <ercanerdogan@gmail.com>
2019-09-16 01:14:02 +0300

    add component example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+
+  constructor() { }
+
+  customer:string="Ercan Erdoğan"
+  
+  ngOnInit() {
+  }
+
+}
