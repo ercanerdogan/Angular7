1dc4f3def23d960936bf40f9243850919b6616cf
Ercan-home <ercanerdogan@gmail.com>
2019-09-24 07:51:39 +0300

    HttpClientModule sample - get user post from json into bootstrap Offcanva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { NavComponent } from './nav/nav.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
-    NavComponent
+    NavComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
