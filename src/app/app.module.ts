9c936a2ea17bc1e350ca719c85be0f2848a001e9
Ercan-home <ercanerdogan@gmail.com>
2019-10-03 08:22:01 +0300

    Routes, RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
 import { NavComponent } from './nav/nav.component';
 import { PostComponent } from './post/post.component';
+
+const routes:Routes=[
+  { path:"posts", component:PostComponent },
+  { path:"", redirectTo:"posts", pathMatch:"full"},
+  { path:"customers", component:CustomerComponent},
+  { path:"posts/:userID", component:PostComponent}
+]
+
 
 @NgModule({
   declarations: [
@@ -16,7 +26,8 @@ import { PostComponent } from './post/post.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule 
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
